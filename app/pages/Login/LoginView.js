b85e0456bd4f0b87ca31809724f49d03cdc02b4c
chuhan <384840951@qq.com>
2016-10-02 01:52:40 +0800

    添加登录

--- app/pages/Login/LoginView.js
+++ app/pages/Login/LoginView.js
@@ -8,17 +8,57 @@ import {
     TouchableOpacity
 } from 'react-native';
 
-import {BGColor, BTNColor, Screen, pixelRation} from '../../utils/CommonUtil';
+import {connect} from 'react-redux';
+import {BGColor, BTNColor, Screen, pixelRation,validateMobile} from '../../utils/CommonUtil';
 import NavBar from '../../components/DefaultNavBar';
-import  LabelInput from '../../components/LabelInput';
+import LabelInput from '../../components/LabelInput';
 import Button from '../../components/Button';
 import AgreementView from './AgreementView';
-import RegistView from './RegistView';
+//import ForgetView from './ForgetView';
+import RegistView from './RegistView';   //注册页面
+import Toast from 'react-native-root-toast';
+import {updateLogin, loginSubim} from '../../actions/loginAction'
 
+import UserDefaults from '../../utils/GlobalStorage';
 
-export default class LoginView extends Component {
+class LoginView extends Component {
+
+    componentDidMount(){
+        //获取缓存中的手机号码 你没存啊 没登陆啊..那个回显 传值都不是很清楚啊  哪个等下
+        UserDefaults.objectForKey("userInfo",(data)=> {
+            if (data) {
+                alert(data["LOGIN_USER_ID"])
+            }
+             //去缓存 是在这操作的吗 那我取多个不就要一直回调了吗 你需要取多个的  通过 字典 或者 数组存啊 这样不好取呀
+        });
+    }
+
+    loginClick = () => {
+        const {dispatch} = this.props;
+        const {userID, password} = this.props.login;
+        if(userID == undefined || userID ==""){
+            Toast.show("请输入手机号",{
+                duration:2000,
+            });
+            return;
+        }
+        if(!validateMobile(userID)){
+            Toast.show("手机格式不正确",{
+                duration:2000,
+            })
+            return;
+        }
+        if(password==undefined || password == '' ){
+            Toast.show("请输入密码",{
+                duration:2000,
+            })
+            return;
+        }
+        dispatch(loginSubim(userID, password));
+    }
 
     render() {
+        const {dispatch} = this.props;
         //iosMode returnKeyType 确认键的内容 enablesReturnKeyAutomatically 为空时禁用按钮 clearButtonMode 右侧显示删除
         //  const { label,labelStyle,textStyle,placeholder,keyboardType,inputType,max,iosMode}  = this.props;
         return (
@@ -30,57 +70,83 @@ export default class LoginView extends Component {
                             this.props.navigator.push({
                                 component: RegistView
                             })
-                        }}/>
+                        }}
+                        onBack={()=> {
+                            this.props.navigator.pop();
+                        }}
+                />
                 {/* <Image style={{flex:1,width:Screen.width,height:Screen.height}}
                  source={require('../../image/0.jpg')}
                  resizeMode="cover"> */}
-                <View style={styles.container}>
-                    <LabelInput label="手机号"
-                                labelStyle={{width: 50}}
-                                placeholder="请输入用户名"
-                                defaultValue=""
-                                max={11}
-                                keyboardType="numeric"
-                                onChangeText={(text) => {console.log(text)}}
 
-                    />
+                <View style={{justifyContent: 'space-between', flex: 1}}>
+                    <View>
+                        <View style={styles.container}>
+                            <LabelInput label="手机号"
+                                        labelStyle={{width: 50}}
+                                        placeholder="请输入用户名"
+                                        defaultValue=""
+                                        max={11}
+                                        keyboardType="numeric"
+                                        onChangeText={(text) => {
+                                            dispatch(updateLogin({userID: text}))
+                                        }}
 
-                    <LabelInput label="密码"
-                                labelStyle={{width: 50}}
-                                textStyle={{borderBottomWidth: 0}}
-                                placeholder="请输入密码"
-                                defaultValue=""
-                                type={true}
-                    />
+                            />
+
+                            <LabelInput label="密码"
+                                        labelStyle={{width: 50}}
+                                        textStyle={{borderBottomWidth: 0}}
+                                        placeholder="请输入密码"
+                                        defaultValue=""
+                                        type={true}
+                                        onChangeText={(text) => {
+                                            dispatch(updateLogin({password: text}))
+                                        }}
+                            />
+                        </View>
+
+                        <TouchableOpacity style={styles.find}
+                                          onPress={()=>{
+                                              {/*this.props.navigator.push({*/}
+                                                  {/*component:ForgetView*/}
+                                              {/*})*/}
+                                          }}>
+                            <Text style={{marginTop: 10, paddingRight: 10, marginBottom: 30}}>找回密码</Text>
+                        </TouchableOpacity>
+
+                        <Button text="登  录"
+                                style={{
+                                    width: Screen.width - 40,
+                                    backgroundColor: BTNColor,
+                                    alignSelf: "center",
+                                    borderRadius: 8
+                                }}
+                                onPress={this.loginClick}
+                        />
+                    </View>
+                    <Button text="东风日产车主APP使用协议"
+                            textStyle={{color: "#666", fontWeight: "300"}}
+                            style={{marginBottom: 5, backgroundColor: BGColor}}
+                            onPress={()=> {
+                                this.props.navigator.push({
+                                    component: AgreementView
+                                })
+                            }}/>
                 </View>
 
-                <TouchableOpacity style={styles.find}>
-                    <Text style={{marginTop: 10, paddingRight: 10, marginBottom: 30}}>找回密码</Text>
-                </TouchableOpacity>
-
-                <Button text="登  录"
-                        style={{
-                            width: Screen.width - 40,
-                            backgroundColor: BTNColor,
-                            alignSelf: "center",
-                            borderRadius: 8
-                        }}
-                        onPress={()=> {
-                            this.login
-                        }}/>
-                <Button text="东风日产车主APP使用协议"
-                        textStyle={{color: "#666", fontWeight: "300"}}
-                        style={{marginBottom: 10, alignSelf: "flex-end", backgroundColor: BGColor}}
-                        onPress={()=> {
-                            this.props.navigator.push({
-                                component: AgreementView
-                            })
-                        }}/>
-                {/* </Image> */}
             </View>
         )
     }
 }
+
+export default connect((state) => {
+    const {login} = state;
+    return {
+        login
+    }
+})(LoginView);
+
 const styles = StyleSheet.create({
     find: {
         alignSelf: "flex-end"
@@ -90,6 +156,7 @@ const styles = StyleSheet.create({
     },
     page: {
         backgroundColor: BGColor,
+        flex: 1
     },
     container: {
         backgroundColor: "#fff",
--- app/pages/Login/RegistView.js
+++ app/pages/Login/RegistView.js
@@ -5,82 +5,148 @@ import {
     Text,
     TextInput,
     Image,
+    Linking,
     TouchableOpacity
 } from 'react-native';
 
-import {BGColor,BTNColor,Screen,pixelRation} from '../../utils/CommonUtil';
+import {connect} from 'react-redux';
+import {BGColor, BTNColor, Screen, pixelRation,GM_CALL, validateMobile} from '../../utils/CommonUtil';
 import NavBar from '../../components/DefaultNavBar';
 import LabelInput from '../../components/LabelInput';
 import Button from '../../components/Button';
-import AgreementView from './AgreementView';
+import AgreementView from './AgreementView';    //注册协议
+import Toast from 'react-native-root-toast';
+import {updateRegist} from '../../actions/registAction'
 
-export default class RegistView extends Component{
-    render(){
-        return(
+class RegistView extends Component {
+
+    getVailidateCode = ()=> {
+        //这个页面在哪里
+        const {dispatch} = this.props;
+        const {mobile} =this.props.regist;
+        if(mobile == undefined || mobile ==""){
+            Toast.show("请输入手机号",{
+                duration:2000,
+            });
+            return;
+        }
+        if(!validateMobile(mobile)){
+            Toast.show("手机格式不正确",{
+                duration:2000,
+            })
+            return;
+        }
+        alert(mobile);
+    }
+
+    render() {
+        const {dispatch} = this.props;
+        return (
             <View style={styles.page}>
                 <NavBar title="注册"
-                         />
-                <View style={styles.container}>
-                <LabelInput  label="手机号"
-                             labelStyle={{width:60}}
-                             placeholder="请输入用户名"
-                             defaultValue=""
-                             max={11}
-                             keyboardType="numeric"
-                              >
-               <button text="发送验证码"></button>
-                              </LabelInput>
-               <LabelInput  label="验证码"
-                            labelStyle={{width:60}}
-                            placeholder="请输入用户名"
-                            defaultValue=""
-                            max={6}
-                            keyboardType="numeric"
-                              />
-              <LabelInput  label="密码"
-                           labelStyle={{width:60}}
-                           placeholder="请输入用户名"
-                           defaultValue=""
-                           type={true}
-                           max={20}
-                             />
-             <LabelInput  label="确认密码"
-                          labelStyle={{width:60}}
-                          placeholder="请输入用户名"
-                          defaultValue=""
-                          type={true}
-                          max={20}
+                />
+                <View style={{justifyContent: 'space-between', flex: 1}}>
+                    <View>
+                        <View style={styles.container}>
+                            <LabelInput label="手机号"
+                                        labelStyle={{width: 60}}
+                                        placeholder="请输入手机号"
+                                        defaultValue=""
+                                        max={11}
+                                        keyboardType="numeric"
+                                        onChangeText={(text)=> {
+                                            dispatch(updateRegist({mobile: text}))
+                                        }}
+                            >
+                            </LabelInput>
+                            <LabelInput label="验证码"
+                                        labelStyle={{width: 60}}
+                                        placeholder="请输入验证码"
+                                        defaultValue=""
+                                        max={6}
+                                        keyboardType="numeric"
                             />
-            </View>
-            <TouchableOpacity style={styles.find}>
-                    <Text style={{marginTop:10,paddingRight:10,marginBottom:30}}>注册遇到问题,联系客服</Text>
-            </TouchableOpacity>
 
-            <Button text="注  册"
-                    style={{width:Screen.width-40,backgroundColor:"#007AFF",alignSelf:"center",borderRadius:8}}
-                    onPress={()=>{alert(1)}} />
-            <Button text="东风日产车主APP使用协议"
-                    textStyle={{color:"#666",fontWeight:"300"}}
-                    style={{marginBottom:10,alignSelf:"center",backgroundColor:BGColor}}
-                    onPress={()=>{
-                        this.props.navigator.push({
-                            component:AgreementView
-                        })
-                    }} />
+                            <LabelInput label="密码"
+                                        labelStyle={{width: 60}}
+                                        placeholder="请输入8-12位密码,可以包括数字和字母"
+                                        defaultValue=""
+                                        type={true}
+                                        max={20}
+                            />
+                            <LabelInput label="确认密码"
+                                        labelStyle={{width: 60}}
+                                        placeholder="请输入8-12位密码,可以包括数字和字母"
+                                        defaultValue=""
+                                        type={true}
+                                        max={20}
+                            />
+                            <TouchableOpacity style={styles.validateBtn}
+                                              onPress={this.getVailidateCode}>
+                                <Text>发送验证码</Text>
+                            </TouchableOpacity>
+                        </View>
 
+                        <TouchableOpacity style={styles.find}
+                                            onPress={()=>{
+                                                Linking.openURL(`tel:${GM_CALL}`);
+                                            }}>
+                            <Text style={{marginTop: 10, paddingRight: 10, marginBottom: 30}}>注册遇到问题,联系客服</Text>
+                        </TouchableOpacity>
+
+                        <Button text="注  册"
+                                style={{
+                                    width: Screen.width - 40,
+                                    backgroundColor: "#007AFF",
+                                    alignSelf: "center",
+                                    borderRadius: 8
+                                }}
+                                onPress={()=> {
+                                    alert(1)
+                                }}/>
+                    </View>
+                    <Button text="东风日产车主APP使用协议"
+                            textStyle={{color: "#666", fontWeight: "300"}}
+                            style={{marginBottom: 10, alignSelf: "center", backgroundColor: BGColor}}
+                            onPress={()=> {
+                                this.props.navigator.push({
+                                    component: AgreementView
+                                })
+                            }}/>
+                </View>
             </View>
         )
     }
 }
 
+export default connect((state) => {
+    const {regist} = state;
+    return {
+        regist
+    }
+})(RegistView);
+
 const styles = StyleSheet.create({
-      page:{
-        backgroundColor:BGColor,
-      },
-      container:{
-        backgroundColor:"#fff",
-        flex:1,
-        marginTop:30,
-        paddingLeft:30,
-      }
+    find: {
+        alignSelf: "flex-end"
+    },
+    page: {
+        backgroundColor: BGColor,
+        flex: 1
+    },
+    container: {
+        backgroundColor: "#fff",
+        flex: 1,
+        marginTop: 30,
+        paddingLeft: 30,
+    },
+    validateBtn: {
+        borderColor: "#b2b2b2",
+        borderWidth: 1,
+        padding: 5,
+        borderRadius: 10,
+        position: "absolute",
+        top: 60,
+        right: 10
+    }
 })
