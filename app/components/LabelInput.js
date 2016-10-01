40f036588fbe7d1fb371dc54d0c64a28cd958c38
少言 <hanhun@163.com>
2016-10-01 11:07:09 +0800

    onChangeText

--- app/components/LabelInput.js
+++ app/components/LabelInput.js
@@ -1,76 +1,78 @@
-import React, {Component,PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {
     StyleSheet,
     View,
     Text,
     TextInput,
-    Platform ,
+    Platform,
     TouchableOpacity
 } from 'react-native';
 
 import {pixelRation} from '../utils/CommonUtil';
-export default class LabelInput extends Component{
-   static PropTypes = {
-     label:PropTypes.string,
-     labelStyle:PropTypes.object,
-     textStyle:PropTypes.object,
-     placeholder:PropTypes.string,
-     keyboardType:PropTypes.numeric,
-     max:PropTypes.number,
-     type:PropTypes.bool,
-     returnKeyType:PropTypes.numeric,
-     defaultValue:PropTypes.string
-   }
+export default class LabelInput extends Component {
+    static PropTypes = {
+        label: PropTypes.string,
+        labelStyle: PropTypes.object,
+        textStyle: PropTypes.object,
+        placeholder: PropTypes.string,
+        keyboardType: PropTypes.numeric,
+        max: PropTypes.number,
+        type: PropTypes.bool,
+        returnKeyType: PropTypes.numeric,
+        defaultValue: PropTypes.string,
+        onChangeText: PropTypes.func,
+    }
 
-   static defaultProps ={
-      label:"",
-      placeholder :"",
-      keyboardType :"default",
-      returnKeyType:"done",
-      type:false,
-      max:20
-   }
+    static defaultProps = {
+        label: "",
+        placeholder: "",
+        keyboardType: "default",
+        returnKeyType: "done",
+        type: false,
+        max: 20,
+        onChangeText: () => {},
+    }
 
-    render(){
-      //iosMode returnKeyType 确认键的内容 enablesReturnKeyAutomatically 为空时禁用按钮 clearButtonMode 右侧显示删除
-     const { label,labelStyle,textStyle,placeholder,defaultValue,returnKeyType,keyboardType,type,max}  = this.props;
+    render() {
+        //iosMode returnKeyType 确认键的内容 enablesReturnKeyAutomatically 为空时禁用按钮 clearButtonMode 右侧显示删除
+        const {label, labelStyle, textStyle, placeholder, defaultValue, returnKeyType, keyboardType, type, max,onChangeText}  = this.props;
 
-      return(
-              <View style={{flex:1}}>
-                <View style={[styles.container,textStyle]}>
-                   <Text style={[styles.label,labelStyle]}>{label}</Text>
-                      <TextInput placeholder={placeholder}
-                                 style={styles.textInput}
-                                 maxLength ={max}
-                                 clearButtonMode = "while-editing"
-                                 enablesReturnKeyAutomatically = {true}
-                                 secureTextEntry ={type}
-                                 defaultValue = {defaultValue}
-                                 keyboardType ={keyboardType}
-                                 returnKeyType ={returnKeyType}
-                                 underlineColorAndroid = "transparent"
-                                 >
-                      </TextInput>
+        return (
+            <View style={{flex: 1}}>
+                <View style={[styles.container, textStyle]}>
+                    <Text style={[styles.label, labelStyle]}>{label}</Text>
+                    <TextInput placeholder={placeholder}
+                               style={styles.textInput}
+                               maxLength={max}
+                               clearButtonMode="while-editing"
+                               enablesReturnKeyAutomatically={true}
+                               secureTextEntry={type}
+                               defaultValue={defaultValue}
+                               keyboardType={keyboardType}
+                               returnKeyType={returnKeyType}
+                               underlineColorAndroid="transparent"
+                               onChangeText={ (text) => {onChangeText(text)}}
+                    >
+                    </TextInput>
                 </View>
-              </View>
-      )
+            </View>
+        )
     }
 }
 
 const styles = StyleSheet.create({
-      label:{
-      },
-      textInput:{
-        flex:1,
-        marginLeft:10,
-        borderWidth:0,
-      },
-      container:{
-        flex:1,
-        flexDirection:"row",
-        borderBottomWidth:1/pixelRation,
-        borderBottomColor:"#ebebeb",
-        justifyContent:"center",
-        alignItems:"center",
-      }
+    label: {},
+    textInput: {
+        flex: 1,
+        marginLeft: 10,
+        borderWidth: 0,
+    },
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        borderBottomWidth: 1 / pixelRation,
+        borderBottomColor: "#ebebeb",
+        justifyContent: "center",
+        alignItems: "center",
+    }
 })
--- app/pages/Login/LoginView.js
+++ app/pages/Login/LoginView.js
@@ -8,7 +8,7 @@ import {
     TouchableOpacity
 } from 'react-native';
 
-import {BGColor,BTNColor,Screen,pixelRation} from '../../utils/CommonUtil';
+import {BGColor, BTNColor, Screen, pixelRation} from '../../utils/CommonUtil';
 import NavBar from '../../components/DefaultNavBar';
 import  LabelInput from '../../components/LabelInput';
 import Button from '../../components/Button';
@@ -16,78 +16,86 @@ import AgreementView from './AgreementView';
 import RegistView from './RegistView';
 
 
-export default class LoginView extends Component{
+export default class LoginView extends Component {
 
-    render(){
-      //iosMode returnKeyType 确认键的内容 enablesReturnKeyAutomatically 为空时禁用按钮 clearButtonMode 右侧显示删除
-     //  const { label,labelStyle,textStyle,placeholder,keyboardType,inputType,max,iosMode}  = this.props;
-      return(
-          <View style={styles.page}>
-              <NavBar title="登录"
-                      rightText="注册"
-                      hideRightButton ={false}
-                      onRightClick={()=>{
-                          this.props.navigator.push({
-                              component:RegistView
-                          })
-                      }} />
-              {/* <Image style={{flex:1,width:Screen.width,height:Screen.height}}
-                     source={require('../../image/0.jpg')}
-                     resizeMode="cover"> */}
-              <View style={styles.container}>
-              <LabelInput label="手机号"
-                        labelStyle={{width:50}}
-                        placeholder="请输入用户名"
-                        defaultValue=""
-                        max={11}
-                        keyboardType="numeric"
-                         />
+    render() {
+        //iosMode returnKeyType 确认键的内容 enablesReturnKeyAutomatically 为空时禁用按钮 clearButtonMode 右侧显示删除
+        //  const { label,labelStyle,textStyle,placeholder,keyboardType,inputType,max,iosMode}  = this.props;
+        return (
+            <View style={styles.page}>
+                <NavBar title="登录"
+                        rightText="注册"
+                        hideRightButton={false}
+                        onRightClick={()=> {
+                            this.props.navigator.push({
+                                component: RegistView
+                            })
+                        }}/>
+                {/* <Image style={{flex:1,width:Screen.width,height:Screen.height}}
+                 source={require('../../image/0.jpg')}
+                 resizeMode="cover"> */}
+                <View style={styles.container}>
+                    <LabelInput label="手机号"
+                                labelStyle={{width: 50}}
+                                placeholder="请输入用户名"
+                                defaultValue=""
+                                max={11}
+                                keyboardType="numeric"
+                                onChangeText={(text) => {console.log(text)}}
 
-               <LabelInput label="密码"
-                           labelStyle={{width:50}}
-                           textStyle={{borderBottomWidth:0}}
-                           placeholder="请输入密码"
-                           defaultValue=""
-                           type={true}
-                            />
-              </View>
+                    />
 
-              <TouchableOpacity style={styles.find}>
-                      <Text style={{marginTop:10,paddingRight:10,marginBottom:30}}>找回密码</Text>
-              </TouchableOpacity>
+                    <LabelInput label="密码"
+                                labelStyle={{width: 50}}
+                                textStyle={{borderBottomWidth: 0}}
+                                placeholder="请输入密码"
+                                defaultValue=""
+                                type={true}
+                    />
+                </View>
 
-              <Button text="登  录"
-                      style={{width:Screen.width-40,backgroundColor:BTNColor,alignSelf:"center",borderRadius:8}}
-                      onPress={()=>{this.login}} />
-              <Button text="东风日产车主APP使用协议"
-                      textStyle={{color:"#666",fontWeight:"300"}}
-                      style={{marginBottom:10,alignSelf:"flex-end",backgroundColor:BGColor}}
-                      onPress={()=>{
-                          this.props.navigator.push({
-                              component:AgreementView
-                          })
-                      }} />
-              {/* </Image> */}
-          </View>
-      )
+                <TouchableOpacity style={styles.find}>
+                    <Text style={{marginTop: 10, paddingRight: 10, marginBottom: 30}}>找回密码</Text>
+                </TouchableOpacity>
+
+                <Button text="登  录"
+                        style={{
+                            width: Screen.width - 40,
+                            backgroundColor: BTNColor,
+                            alignSelf: "center",
+                            borderRadius: 8
+                        }}
+                        onPress={()=> {
+                            this.login
+                        }}/>
+                <Button text="东风日产车主APP使用协议"
+                        textStyle={{color: "#666", fontWeight: "300"}}
+                        style={{marginBottom: 10, alignSelf: "flex-end", backgroundColor: BGColor}}
+                        onPress={()=> {
+                            this.props.navigator.push({
+                                component: AgreementView
+                            })
+                        }}/>
+                {/* </Image> */}
+            </View>
+        )
     }
 }
 const styles = StyleSheet.create({
-      find:{
-        alignSelf:"flex-end"
-      },
-      regist:{
-        alignSelf:"flex-start"
-      },
-      page:{
-        backgroundColor:BGColor,
-      },
-      container:{
-        backgroundColor:"#fff",
-        flex:1,
-        marginTop:30,
-        paddingLeft:30,
-      },
-      textStyle:{
-      }
+    find: {
+        alignSelf: "flex-end"
+    },
+    regist: {
+        alignSelf: "flex-start"
+    },
+    page: {
+        backgroundColor: BGColor,
+    },
+    container: {
+        backgroundColor: "#fff",
+        flex: 1,
+        marginTop: 30,
+        paddingLeft: 30,
+    },
+    textStyle: {}
 })
