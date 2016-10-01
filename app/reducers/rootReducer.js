cc65cf05de38380bd2d94f979418302be49a3520
chuhan <384840951@qq.com>
2016-10-02 01:53:03 +0800

    添加登录

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -5,8 +5,11 @@
 import { combineReducers } from 'redux'
 import fields from './fieldsReducer'
 import home from './homeReducer'
+import login from './loginReducer'
+import regist from './registReducer'
 
 export default rootReducer = combineReducers({
     fields,
-    home
+    home,
+    login
 })
