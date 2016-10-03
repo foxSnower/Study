e32f13c2b353380d0382b3cd3f7353957902b3be
litao <litaowork@aliyun.com>
2016-10-03 10:22:07 +0800

    修复wikiReducer不在rootReducer内问题

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -6,9 +6,11 @@ import { combineReducers } from 'redux'
 import fields from './fieldsReducer'
 import home from './homeReducer'
 import login from './loginReducer'
+import wiki from './wikiReducer'
 
 export default rootReducer = combineReducers({
     fields,
     home,
-    login
+    login,
+    wiki
 })
